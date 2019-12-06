b460bb3b1f176398376eba17e0928232c6a192ba
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-12-06 18:27:56 +0800

    Auto-enable parallel build

--- build-package.sh
+++ build-package.sh
@@ -2,4 +2,5 @@
 
 mk-build-deps -i -r -t "apt-get -y" /tmp/package/Eigen3ToPython/debian/control
 cd /package/Eigen3ToPython
+sed -i -e's/dh $@\s*$/dh $@ --parallel/' debian/rules
 debuild --no-tgz-check -i -I -b -us -uc
