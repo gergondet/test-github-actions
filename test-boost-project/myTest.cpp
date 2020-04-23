848dec211fb09177f40fa2381b5d6b5569307c8b
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-04-23 13:51:26 +0800

    Try to build a simple Boost project

---
+++ test-boost-project/myTest.cpp
@@ -0,0 +1,14 @@
+// boost
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE test
+#include <boost/test/unit_test.hpp>
+#include <boost/filesystem.hpp>
+
+namespace bfs = boost::filesystem;
+
+BOOST_AUTO_TEST_CASE(MyTest)
+{
+  bfs::path path("/NOT/EVEN");
+  BOOST_REQUIRE(!bfs::exists(path));
+}
+
