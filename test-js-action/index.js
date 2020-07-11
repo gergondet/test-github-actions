f26b844cc50c70e7b8869b52d7c9a4b636586f6c
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-07-11 14:12:01 +0800

    Checkout in test-action

--- test-js-action/index.js
+++ test-js-action/index.js
@@ -31,6 +31,7 @@ async function run()
     const docker_images = yaml.safeLoad(core.getInput('docker-images'));
     console.log('with-docker ${with_docker}');
     console.log('docker-images ${docker_images}');
+    docker_images.forEach(img => console.log('Would build on ${img}'));
   }
   catch(error)
   {
