462f39238f42d3051e0cb890771eee900ffcd00e
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-07-11 14:09:05 +0800

    Load docker-images as yaml

--- test-js-action/index.js
+++ test-js-action/index.js
@@ -2,6 +2,7 @@ const { boolean } = require('boolean');
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const github = require('@actions/github');
+const yaml = require('js-yaml');
 
 async function bash(cmd)
 {
@@ -27,7 +28,7 @@ async function run()
   {
     // Get options
     const with_docker = core.getInput('with-docker');
-    const docker_images = core.getInput('docker-images');
+    const docker_images = yaml.safeLoad(core.getInput('docker-images'));
     console.log('with-docker ${with_docker}');
     console.log('docker-images ${docker_images}');
   }
