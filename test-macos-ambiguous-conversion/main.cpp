201539ae3560e6eae6f5ced7eed912ba563e8ae6
Pierre Gergondet <pierre.gergondet@gmail.com>
2021-04-15 11:56:04 +0800

    Add a conversion test for macos

---
+++ test-macos-ambiguous-conversion/main.cpp
@@ -0,0 +1,21 @@
+#include <cstdint>
+#include <vector>
+
+struct Foo
+{
+  operator int() const
+  {
+    return 0;
+  }
+  operator uint64_t() const
+  {
+    return 0;
+  }
+};
+
+int main()
+{
+  size_t s = Foo{};
+  static_assert(std::is_same_v<uint64_t, size_t>, "OK");
+  return s;
+}
